8c8249626dc3d50463d4ad89e75fb084faf46ba8
Ibrahim Mkusa <ibrahimmkusa@gmail.com>
2023-06-19 12:40:23 -0400

    ex4.4 more unit tests and helper functions with jest

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,5 +1,18 @@
+const logger = require('../utils/logger.js')
 const dummy = (blogs) => {
   return 1
 }
 
-module.exports = {dummy}
+
+const totalLikes = (blogs) => {
+  likes = 0
+  for (const blog of blogs) {
+    likes += blog.likes
+  }
+
+  return likes
+}
+
+module.exports = {
+  dummy, totalLikes
+}
