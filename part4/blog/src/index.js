81c9e17aebc95f0939c5b636ca64af9d47dfc992
Ibrahim Mkusa <ibrahimmkusa@gmail.com>
2023-06-18 20:26:14 -0400

    ex4.2 tackling complexity via modularity

--- part4/blog/src/index.js
+++ part4/blog/src/index.js
@@ -1,47 +1,25 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const Blog = require('../models/blog.js')
+const blogsRouter = require('../controllers/blogs')
 const mongoose = require('mongoose')
+const logger = require('../utils/logger.js')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-console.log("Connecting to mongo server")
-const mongoUrl = 'somewhereLOL'
+logger.info("Connecting to mongo server")
+const mongoUrl = 'hello'
 mongoose.connect(mongoUrl)
-console.log("Successfully connected to mongo server")
+logger.info("Successfully connected to mongo server")
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-    .catch((error) => {
-      console.log(error.message)
-    })
-})
+app.get('/api/blogs', blogsRouter)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.post('/api/blogs', blogsRouter)
 
 const PORT = 3003
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+ logger.info(`Server running on port ${PORT}`)
 })
---
+++ part4/blog/utils/logger.js
@@ -0,0 +1,11 @@
+const info = (...params) => {
+  console.log('info: ', ...params)
+}
+
+const error = (...params) => {
+  console.log('error: ', ...params)
+}
+
+module.exports = {
+  info, error
+}
